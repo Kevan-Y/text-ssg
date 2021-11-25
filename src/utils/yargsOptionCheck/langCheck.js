1b125ac7b23bbfcabd2d8beb6178ec7eeccf3481
Kevan-Y <58233223+Kevan-Y@users.noreply.github.com>
2021-11-25 16:31:08 -0500

    Convert json to js for languageCode

--- src/utils/yargsOptionCheck/langCheck.js
+++ src/utils/yargsOptionCheck/langCheck.js
@@ -1,15 +1,13 @@
-const fs = require('fs');
-
+// const fs = require('fs');
+const { languageCode } = require('../../languageData/languageCode');
 /**
  * Check if valide lang code
  * @param {string} argv - lang code
  * @return {boolean}
  */
 const langCheck = (argv) => {
-  const rawData = fs.readFileSync('src/languageData/languageCode.json');
-  const langCode = JSON.parse(rawData);
-  langCode.lang = langCode.lang.map((lang) => lang.toLowerCase());
-  if (langCode.lang.includes(argv.toLowerCase())) {
+  const langCode = languageCode.map((lang) => lang.toLowerCase());
+  if (langCode.includes(argv.toLowerCase())) {
     return true;
   }
   throw new Error('Must be an valid code language.');
